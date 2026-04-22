62bc1101405612d5bf203bd0588288dfc657ee33
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-04-21 19:01:21 -0500

    TUnit conversion

--- test/Soenneker.Blazor.Utils.BlazorInvoker.Tests/BlazorInvokerTests.cs
+++ test/Soenneker.Blazor.Utils.BlazorInvoker.Tests/BlazorInvokerTests.cs
@@ -1,17 +1,15 @@
-using Soenneker.Tests.FixturedUnit;
-using Xunit;
-
+using Soenneker.Tests.HostedUnit;
 
 namespace Soenneker.Blazor.Utils.BlazorInvoker.Tests;
 
-[Collection("Collection")]
-public class BlazorInvokerTests : FixturedUnitTest
+[ClassDataSource<Host>(Shared = SharedType.PerTestSession)]
+public class BlazorInvokerTests : HostedUnitTest
 {
-    public BlazorInvokerTests(Fixture fixture, ITestOutputHelper output) : base(fixture, output)
+    public BlazorInvokerTests(Host host) : base(host)
     {
     }
 
-    [Fact]
+    [Test]
     public void Default()
     {
 
