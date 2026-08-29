83259d0892ce48e534cd919d4bb0e74163da0a6f
soenneker <jake@soenneker.com>
2026-08-29 14:50:16 -0500

    Apply interface XML documentation conventions

--- src/Soenneker.Blazor.Utils.BlazorInvoker/BlazorInvoker.cs
+++ src/Soenneker.Blazor.Utils.BlazorInvoker/BlazorInvoker.cs
@@ -7,7 +7,7 @@ using Soenneker.Extensions.ValueTask;
 
 namespace Soenneker.Blazor.Utils.BlazorInvoker;
 
-///<inheritdoc cref="IBlazorInvoker{TInput}"/>
+/// <inheritdoc cref="IBlazorInvoker{TInput}"/>
 public sealed class BlazorInvoker<TInput> : IBlazorInvoker<TInput>
 {
     private readonly Func<TInput, ValueTask> _func;
